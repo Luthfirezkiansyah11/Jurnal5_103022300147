20abafbebefa319ceb917e99b0e6836634da36e3
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 17:08:31 +0700

    Menambahkan Class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,5 +1,35 @@
 ﻿namespace Jurnal5_103022300147
+
 {
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            dynamic temp = (dynamic)data;
+            dynamic tempDate = (dynamic)DateTime.Now;
+            storedData.Add(temp);
+            inputDates.Add(tempDate);
+
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC {inputDates[i]}");
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
